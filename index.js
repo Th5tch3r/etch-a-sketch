28e938a3d52069091f557c5b799d62576c11d422
Thatcher <thatcher@Thatchers-iMac.local>
2023-08-02 23:59:25 -0400

    Able to create 16x16 grid, need to re-format

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const container = document.querySelector(".container")
-const grid = document.createElement("div");
-container.appendChild(grid);
+// const grid = document.createElement("div");
+// container.appendChild(grid);
+
+// create a function to make desired grid
+function makeGrid(rows, columns) {
+        container.style.setProperty("--grid-rows", rows);
+        container.style.setProperty("--grid-columns", columns);
+        for (let i = 0; i < (rows * columns); i++ ) {
+                let box = document.createElement("div");
+                container.appendChild(box).className = "grid-box";
+        }
+}
+
+makeGrid(16, 16);
